2792b0ebb09de0ee36f880f18bbfdfaeb8f11f08
knutinator <jonatanknut@gmail.com>
2023-01-26 08:46:14 +0000

    add random number generator for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,15 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
-
+/**main game loop */
 function runGame() {
 
+let num1 = Math.floor(Math.random() * 25) + 1;
+let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {}
+
 
 function calculateCorrectAnswer() {}
 
