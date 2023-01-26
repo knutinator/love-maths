48f0ffc1e100b21881c39aa4970ec97d36e16cf6
knutinator <jonatanknut@gmail.com>
2023-01-26 10:20:09 +0000

    add code for score tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
-        alert(`Awwwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer}!`);
+        alert(`Awwwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -57,9 +59,18 @@ function calculateCorrectAnswer() {
     }
 }
 
-function incrementScore() {}
+function incrementScore() {
 
-function incrementWrongAnswer() {}
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
+}
+
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+}
 
 function displayAdditionQuestion(operand1, operand2) {
 
